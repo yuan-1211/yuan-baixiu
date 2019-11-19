0e7f285df9932518315e6455587f807fb230629f
yuan-1211 <1012314087@qq.com>
2019-11-19 18:39:53 +0800

    评论

--- public/assets/js/detail.js
+++ public/assets/js/detail.js
@@ -1,5 +1,6 @@
 
 let id =getId('id')
+var preview;
 $.ajax({
     type: "GET",
     url: `/posts/${id}`,
@@ -18,3 +19,48 @@ $('#articleBox').on('click', '#like',function () {
         }
     });
 });
+$.ajax({
+    type: "get",
+    url: "/settings",  
+    success: function (response) {
+        preview=response.preview
+        if(response.comment){
+        var html=template('commentTpl')
+        $('#commentBox1').html(html)
+        }
+    }
+});
+$('.content').on('submit','form',function(){
+    var content = $(this).find('textarea').val();
+	// 代表评论的状态
+	var state;
+		
+	if (preview) {
+		// 要经过人工审核
+		state = 0;
+	}else {
+		// 不需要经过人工审核
+		state = 1;
+	}
+
+	// 向服务器端发送请求 执行添加评论操作
+	$.ajax({
+		type: 'get',
+		url: '/comments',
+		data: {
+			content: content,
+			post: id,
+			state: state
+		},
+		success: function () {
+			alert('评论成功')
+			location.reload();
+		},
+		error: function () {
+			alert('评论失败')
+		}
+	})
+
+	// 阻止表单默认提交行为
+	return false;
+})
