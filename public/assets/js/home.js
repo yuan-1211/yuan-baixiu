6bbf3104eec59a728a86c7f256df149a6c6e5a75
yuan-1211 <1012314087@qq.com>
2019-11-19 09:45:59 +0800

    轮播图

---
+++ public/assets/js/home.js
@@ -0,0 +1,27 @@
+$.ajax({
+    type: "get",
+    url: "/slides",
+    success: function (response) {
+        let html=template('sliderTpl',{data:response})
+        $('#sliderBox').html(html)
+        var swiper = Swipe(document.querySelector('.swipe'), {
+            auto: 3000,
+            transitionEnd: function (index) {
+              // index++;
+      
+              $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
+            }
+          });
+      
+          // 上/下一张
+          $('.swipe .arrow').on('click', function () {
+            var _this = $(this);
+      
+            if(_this.is('.prev')) {
+              swiper.prev();
+            } else if(_this.is('.next')) {
+              swiper.next();
+            }
+          })
+    }
+});
