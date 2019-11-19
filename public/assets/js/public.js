7b3124d030e8a2ebf2bfab3a3fca7e9f81486bd4
yuan-1211 <1012314087@qq.com>
2019-11-19 12:36:50 +0800

    导航栏

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -48,3 +48,21 @@ $.ajax({
         
     }
 });
+$.ajax({
+	type: 'get',
+	url: '/categories',
+	success: function (response) {
+		var navTpl = `
+			{{each data}}
+			<li>
+				<a href="list.html?categoryId={{$value._id}}">
+					<i class="fa {{$value.className}}"></i>{{$value.title}}
+				</a>
+			</li>
+			{{/each}}
+		`;
+		var html = template.render(navTpl, {data: response});
+		$('#navBox').html(html)
+		$('#topNavBox').html(html)  //顶部导航栏（缩小窗口可以看见）
+	}
+})
